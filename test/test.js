eb4595580c5b0b6747f858612a266acddbd84192
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-06-03 15:52:49 -0300

    Update code to include only numeric values as valid input

--- test/test.js
+++ test/test.js
@@ -20,3 +20,35 @@ test('Test 4', t => {
 test('Test 5', t => {
   t.is(sum([27, 421, -8, 0, -154, 23, 11, 256]), 576)
 })
+
+test('Test Infinity', t => {
+  t.is(sum([4, -1024, 6, Infinity]), Infinity)
+})
+
+test('Test -Infinity', t => {
+  t.is(sum([4, 4096, 77, -Infinity]), -Infinity)
+})
+
+test('Test char', t => {
+  const error = t.throws(() => {
+    sum([84, 256, 6, '1', 0, -27])
+  }, Error)
+
+  t.is(error.message, 'Only numeric values are allowed')
+})
+
+test('Test NaN', t => {
+  const error = t.throws(() => {
+    sum([4, 5, 6, NaN])
+  }, Error)
+
+  t.is(error.message, 'Only numeric values are allowed')
+})
+
+test('Test null', t => {
+  const error = t.throws(() => {
+    sum([84, 512, 6, null, 0, -27])
+  }, Error)
+
+  t.is(error.message, 'Only numeric values are allowed')
+})
